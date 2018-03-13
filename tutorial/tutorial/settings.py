a9a757bd117b1c66c1c480a890e3b5fa8a852de0
mgraves <mgraves@mail.fresnostate.edu>
2018-03-13 11:34:18 -0700

    removed static

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'oauth2_provider',
     'rest_framework',
     'snippets.apps.SnippetsConfig',
